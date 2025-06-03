3844337d98e8e921ff408f5a2495d32b39178909
Ethiopica <eliobais@gmail.com>
2025-06-03 18:38:24 +0300

    add on home paage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,30 @@
-const Home = () => {
-  return (
-    <div>
-      <h1>I am Home page</h1>
-    </div>
-  )
-}
+import React from 'react';
+import styles from './Home.module.css';
+import heroImage from '../assets/h.jpg'; 
 
-export default Home
+export default function Home() {
+  const stats = [
+    { number: 574, label: 'Data' },
+    { number: 327, label: 'Services' },
+    { number: 197, label: 'Number of Vists' },
+  ];
+  return (
+    <div className={styles.container}>
+      <section className={styles.hero} style={{ backgroundImage: `url(${heroImage})` }}> 
+        <div className={styles.heroContent}>
+          <h1>Manage your events all year round</h1>
+          <p>Helsinki Event Planner</p> 
+        </div>
+    
+        <div className={styles.statsContainer}>
+          {stats.map((stat, index) => (
+            <div key={index} className={styles.statCard}>
+              <h3>{stat.number}</h3>
+              <p>{stat.label}</p>
+            </div>
+          ))}
+        </div>
+      </section>
+    </div>
+  );
+}
