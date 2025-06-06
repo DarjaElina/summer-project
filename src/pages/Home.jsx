9b995f08b3af8cf33bab31e4aec5100f1508300e
Ethiopica <eliobais@gmail.com>
2025-06-06 14:12:13 +0300

    add

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,35 +1,4 @@
-<<<<<<< HEAD
-import React from 'react';
-import styles from './Home.module.css';
-import heroImage from '../assets/h.jpg'; 
 
-export default function Home() {
-  const stats = [
-    { number: 574, label: 'Data' },
-    { number: 327, label: 'Services' },
-    { number: 197, label: 'Number of Vists' },
-  ];
-  return (
-    <div className={styles.container}>
-      <section className={styles.hero} style={{ backgroundImage: `url(${heroImage})` }}> 
-        <div className={styles.heroContent}>
-          <h1>Manage your events all year round</h1>
-          <p>Helsinki Event Planner</p> 
-        </div>
-    
-        <div className={styles.statsContainer}>
-          {stats.map((stat, index) => (
-            <div key={index} className={styles.statCard}>
-              <h3>{stat.number}</h3>
-              <p>{stat.label}</p>
-            </div>
-          ))}
-        </div>
-      </section>
-    </div>
-  );
-}
-=======
 import styles from "./Home.module.css";
 import heroImage from "../assets/h.jpg";
 import { Link } from "react-router"; 
@@ -97,4 +66,4 @@ export default function Home() {
   );
 }
 
->>>>>>> c3ab63e549eea3caffdde2664e38cb001fce7a04
+
